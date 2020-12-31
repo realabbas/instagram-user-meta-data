43e8e6fadaa75d4a5b74e3b39cf11ac85e07238d
realabbas5 <irizviali@gmail.com>
2020-12-31 21:02:15 +0530

    instadp response data parsing fixed

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 let username = process.argv[2]
-console.log('Profile Showing for '+ username)
+console.log('Profile Showing for ' + username)
 let insta_url = `https://www.instagram.com/${username}/`;
 let profile_url = `https://www.instadp.com/fullsize/${username}`;
-// var fs = require("fs");
 var Xray = require("x-ray");
 var x = Xray();
-
-// axios
-//   .all([axios.get(insta_url), axios.get(profile_url)])
-//   .then(
-//     axios.spread((profile, dp) => {
-//       let user = {};
-//       x(profile, ["script"])(function (err, title) {
-//         let data = JSON.parse(title[3]);
-//         // console.log(JSON.parse(title[3])); // Google
-//         user = {
-//           name: data.name,
-//           username: data.alternateName,
-//           description: data.description.replace("&quot;", '"').trim(),
-//         };
-//       });
-//       x(dp, [".download-btn@href"])(function (err, title) {
-//         // console.log(title);
-//         user.image = title[0];
-//       });
-//       console.log(user);
-//     })
-//   )
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 var request = require("request");
 var options1 = {
@@ -54,13 +28,13 @@ request(options1, function (error, response) {
       name: data.name,
       username: data.alternateName,
       description: data.description
-//.replace("&quot;", '"').trim(),
+      //.replace("&quot;", '"').trim(),
     };
     request(options2, function (err, res) {
       if (err) throw new Error(err);
-      x(res.body, [".download-btn@href"])(function (err, resdata) {
+      x(res.body, ".download-btn@href")(function (err, resdata) {
         // console.log(resdata);
-        user.image = resdata[0];
+        user.image = resdata;
         console.log(user);
       });
     });
